0ee63e0c7a622e12f31be4fd8edb0cd608105872
Ilia Munaev <ilyamunaev@gmail.com>
2025-02-16 16:54:40 +0200

    Add: data structure to header file

--- include/minishell.h
+++ include/minishell.h
@@ -1,37 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/16 16:46:44 by imunaev-          #+#    #+#             */
+/*   Updated: 2025/02/16 16:53:22 by imunaev-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <readline/history.h>
-#include <readline/readline.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <string.h>
 
+// Maximum number of arguments per command.
+# define MAX_ARGS 64
 
-#define MAX_ARGS 64 // ?
-#define MAX_CMDS 10 // ?
-#define DELIM " "
+// Maximum number of commands in a pipeline.
+# define MAX_CMDS 10
 
+// Delimiter used for tokenizing input.
+# define DELIM " "
 
-// simple command structure, used by parser
-typedef struct s_cmd
+/**
+ * @enum e_redir_type
+ * @brief Enumeration for different types of I/O redirection.
+ *
+ * - `R_INPUT`:		`<` Redirects standard input from a file.
+ * - `R_OUTPUT`:	`>` Redirects standard output to a file.
+ * - `R_APPEND`:	`>>` Redirects standard output in append mode.
+ * - `R_HEREDOC`:	`<<` Reads input until a specified delimiter is found.
+ */
+typedef enum e_redir_type
 {
-	char	*cmd;					// Command name (e.g., "ls", "grep")
-	char	**args;					// Array of arguments (e.g., ["-al"])
-	char	*infile;				// Input redirection (e.g., "infile")
-	char	*outfile;				// Output redirection (e.g., "out.txt")
-	char	*error;					// Error redirection (e.g., "errfile") ?? check
-}	t_cmd;
+	R_INPUT,
+	R_OUTPUT,
+	R_APPEND,
+	R_HEREDOC
+}	t_redir_type;
 
-
-// kind of table, read page 3
-// https://www.cs.purdue.edu/homes/grr/SystemsProgrammingBook/Book/Chapter5-WritingYourOwnShell.pdf
-typedef struct s_cmds
+/**
+ * @struct s_redir
+ * @brief Structure for handling I/O redirections.
+ *
+ * - `type`:		The type of redirection.
+ * - `filename`:	For `R_INPUT`, `R_OUTPUT`, or `R_APPEND`,
+ * 					this stores the filename.
+ *					For `R_HEREDOC`, it stores the delimiter string.
+ */
+typedef struct s_redir
 {
-	t_cmd	**cmds;					// arr of t_cmd cmd, for iteration in pipeline, NULL terminated
+	t_redir_type	type;
+	char			*filename;
+}	t_redir;
 
-} t_cmds;
+/**
+ * @struct s_command
+ * @brief Represents a single command in a pipeline.
+ *
+ * - `argv`:		NULL-terminated array of command arguments.
+ * - `in_redir`:	Pointer to input redirection struct, if any.
+ * - `out_redir`:	Pointer to output redirection struct, if any.
+ * - `next`:		Pointer to the next command in the pipeline.
+ *
+ * Example pipeline: `ls -l | grep minishell | wc -l`
+ * - cmd1:	`ls -l`
+ * - cmd2:	`grep minishell`
+ * - cmd3:	`wc -l`
+ * - cmd4:	`NULL` (end of pipeline)
+ */
+typedef struct s_command
+{
+	char				**argv;
+	t_redir				*in_redir;
+	t_redir				*out_redir;
+	struct s_command	*next;
+}	t_command;
 
-#endif // MINISHELL_H
+#endif /* MINISHELL_H */
