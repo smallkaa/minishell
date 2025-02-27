eddd6edf407a97ed6893267b23da8cb2e2ddc925
Ilia Munaev <imunaev-@c1r4p1.hive.fi>
2025-02-27 13:30:14 +0200

    :Update .gitignore, add .vscode/ for ignore

--- src/executor/builtin.c
+++ src/executor/builtin.c
@@ -1,4 +1,46 @@
 #include "minishell.h"
+// exit
+/*
+https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#exit
+
+The exit utility shall cause the shell to exit from
+its current execution environment with the exit status
+specified by the unsigned decimal integer n.
+If the current execution environment is a subshell environment,
+the shell shall exit from the subshell environment with
+the specified exit status and continue in the environment from
+which that subshell environment was invoked; otherwise,
+the shell utility shall terminate with the specified exit status.
+If n is specified, but its value is not between 0 and 255 inclusively, 
+the exit status is undefined.
+
+A trap on EXIT shall be executed before the shell terminates, 
+except when the exit utility is invoked in that trap itself, 
+in which case the shell shall exit immediately.
+
+The exit status shall be n, if specified, except that the behavior 
+is unspecified if n is not an unsigned decimal integer or 
+is greater than 255. Otherwise, the value shall be the exit value of 
+the last command executed, or zero if no command was executed. 
+When exit is executed in a trap action, the last command is 
+considered to be the command that executed immediately 
+preceding the trap action.
+
+void exit(int status);
+
+*/
+
+int	exec_exit(t_cmd *cmd)
+{
+	int	status;
+
+	status = cmd->argv[1];
+	exit(cmd->argv[1]);
+} 
+
+
+
+
 
 // echo
 // cd
