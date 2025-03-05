2cb0a782e6c846037ec2e0c53e55730eb4fc6e43
Ilia Munaev <imunaev-@c1r4p1.hive.fi>
2025-03-05 14:16:56 +0200

    Reafactor: add handle_cd docs

--- src/executor/handle_cd.c
+++ src/executor/handle_cd.c
@@ -1,43 +1,4 @@
 #include "minishell.h"
-/*
-int chdir(const char *path);
-
-The chdir() function shall cause the directory named 
-by the pathname pointed to by the path argument to become
-the current working directory; that is, the starting point 
-for path searches for pathnames not beginning with '/'.
-
-Upon successful completion, 0 shall be returned. 
-Otherwise, -1 shall be returned, the current working directory 
-shall remain unchanged, and errno shall be set to indicate the error.
-
-ERRORS:
-
-1. ENOENT
-bash: cd: gfgf: No such file or directory
-
-2. EACCES
-mkdir restricted_dir
-chmod 000 restricted_dir  # No permissions
-./minishell restricted_dir
-
-bash: cd: test/: Permission denied
-
-3. ENOTDIR 
-bash: cd: executor.c: Not a directory
-
-4. ENAMETOOLONG
-LONG_PATH=$(printf 'a%.0s' {1..5000})  # Create a long path
-./minishell "/tmp/$LONG_PATH"
-
-bash: /tmp/aaaaaa......aaaaaaaa: File name too long
-
-5. ELOOP
-ln -s loop_dir loop_dir  # Create a symlink pointing to itself
-./minishell cd loop_dir
-
-bash: cd: loop_dir: Too many levels of symbolic links
-*/
 
 /**
  * handle_cd - Handles the 'cd' command to change the working directory.
@@ -49,8 +10,9 @@ bash: cd: loop_dir: Too many levels of symbolic links
  * - If the command is **executed inside a pipe**, it exits with the failure status.
  *
  * @param cmd Pointer to the `t_cmd` structure containing command arguments.
+ * 
+ * More: https://pubs.opengroup.org/onlinepubs/009695299/functions/chdir.html
  */
-
 void	handle_cd(t_cmd *cmd)
 {
 	int		status;
