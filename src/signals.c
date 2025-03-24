ac371bf07362e8fd9ac0bf29375c9a553b369c81
Ilia Munaev <ilyamunaev@gmail.com>
2025-03-24 20:02:57 +0200

    Fix: handle_sigint() return previous version

--- src/signals.c
+++ src/signals.c
@@ -24,7 +24,8 @@ rl_redisplay();*/
 void handle_sigint(int sig)
 {
     (void)sig;
-    write(STDOUT_FILENO, "^C\n", 3);
+    // write(STDOUT_FILENO, "^C\n", 3);
+    write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
