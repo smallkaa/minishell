1f827a95e5ae8899b2f44a0c4c2e3047db892880
Ilia Munaev <imunaev-@c1r3p8.hive.fi>
2025-03-24 15:40:14 +0200

    Add: out redirection for builtin and external commands

--- src/executor/exec_in_parent_process.c
+++ src/executor/exec_in_parent_process.c
@@ -1,33 +1,46 @@
 #include "minishell.h"
 
-
 uint8_t exec_in_parent_process(t_cmd *cmd)
 {
-	int		saved_stdin;
+	int		saved_stdout;
+	int		saved_stdin;;
 	uint8_t	exit_status;
 
 	saved_stdin = dup(STDIN_FILENO);
 	if (saved_stdin == -1)
-	{
-		perror("dup");
-		return (EXIT_FAILURE);
-	}
-
+		perror_return("dup #1: exec_in_parent_process", EXIT_FAILURE);
+	saved_stdout = dup(STDOUT_FILENO);
+	if (saved_stdout == -1)
+		perror_return("dup #2: exec_in_parent_process", EXIT_FAILURE);
 	exit_status = apply_redirections(cmd);
-	if (exit_status == (EXIT_FAILURE))
+	if (exit_status == EXIT_FAILURE)
 	{
-		close(saved_stdin);
+		if (close(saved_stdout) == -1)
+			perror_return("close #1: exec_in_parent_process", EXIT_FAILURE);
 		return (EXIT_FAILURE);
 	}
 	exit_status = exec_builtin(cmd);
 
-	// Restore the original STDIN.
-	if (dup2(saved_stdin, STDIN_FILENO) == -1) {
-		perror("dup2");
-		close(saved_stdin);
-		return (EXIT_FAILURE);
+	// Restore STDOUT
+	if (dup2(saved_stdout, STDOUT_FILENO) == -1)
+	{
+		if (close(saved_stdout) == -1)
+			perror_return("close #2: exec_in_parent_process", EXIT_FAILURE);
+		perror_return("dup #3: exec_in_parent_process", EXIT_FAILURE);
 	}
-	close(saved_stdin);
+	if (close(saved_stdout) == -1)
+		perror_return("close: exec_in_parent_process", EXIT_FAILURE);
 
+	// Restore STDIN
+	if (dup2(saved_stdin, STDIN_FILENO) == -1)
+	{
+		if (close(saved_stdin) == -1)
+			perror_return("close #3: exec_in_parent_process", EXIT_FAILURE);
+		perror_return("dup #4: exec_in_parent_process", EXIT_FAILURE);
+
+	}
+	if (close(saved_stdin) == -1)
+		perror_return("close #4: exec_in_parent_process", EXIT_FAILURE);
 	return (exit_status);
 }
+
