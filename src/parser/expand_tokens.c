50ebfc396a7b77867c01017fbc28a34360d307ae
Pavel Vershinin <smallkaa@gmail.com>
2025-03-31 10:56:03 +0300

    Fix: echo 1 | cat -- now works

--- src/parser/expand_tokens.c
+++ src/parser/expand_tokens.c
@@ -1,29 +1,33 @@
 #include "minishell.h"
 
-static void expand_token_value(t_Token *token, t_mshell *ms)
+static void	expand_token_value(t_Token *token, t_mshell *ms)
 {
-    char *expanded;
+	char	*expanded;
 
-    if (!token || !token->value)
-        return;
-    
-    
-    if (token->value[0] == '"' && token->value[ft_strlen(token->value) - 1] == '"')
-    {
-    }
-    else if (token->value[0] == '\'' && token->value[ft_strlen(token->value) - 1] == '\'')
-    {
-    }
-    
-    // Выполняем расширение переменных и тильды
-    expanded = expand_env_variables(token->value, ms);
-    
-    if (expanded)
-    {
-        free(token->value);
-        token->value = expanded;
-    }
+	if (!token || !token->value)
+		return;
+	expanded = expand_env_variables(token->value, ms);
+	if (expanded)
+	{
+		free(token->value);
+		token->value = expanded;
+
+		// Обновление типа токена на основе его значения
+		if (!token->in_single_quotes) // Только если не в одинарных кавычках
+		{
+			if (ft_strcmp(expanded, "|") == 0)
+				token->type = TOKEN_PIPE;
+			else if (ft_strcmp(expanded, "<") == 0)
+				token->type = TOKEN_REDIRECT_IN;
+			else if (ft_strcmp(expanded, ">") == 0)
+				token->type = TOKEN_REDIRECT_OUT;
+			else if (ft_strcmp(expanded, ">>") == 0)
+				token->type = TOKEN_APPEND_OUT;
+			// можно добавить больше проверок, если нужно
+		}
+	}
 }
+
 
 
 void expand_tokens(t_TokenArray *tokens, t_mshell *ms)
