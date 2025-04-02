f798872a7030793cf983986d06dbbcad7831be82
Ilia Munaev <ilyamunaev@gmail.com>
2025-04-02 19:28:37 +0300

    Refactor: apply_in_redirection

--- src/executor/redirections/handlers/apply_in_redirection.c
+++ src/executor/redirections/handlers/apply_in_redirection.c
@@ -1,42 +1,55 @@
 #include "minishell.h"
 
+int	open_input_file(char *filename)
+{
+	int	in_fd;
+
+	in_fd = open(filename, O_RDONLY);
+	if (in_fd < 0)
+	{
+		print_error("-minishell: ");
+		perror(filename);
+	}
+	return (in_fd);
+}
+
+uint8_t	apply_input_fd(int in_fd)
+{
+	if (dup2(in_fd, STDIN_FILENO) == -1)
+	{
+		if (close(in_fd) == -1)
+			perror_return("-minishell: close", EXIT_FAILURE);
+		perror_return("-minishell: dup2", EXIT_FAILURE);
+	}
+	if (close(in_fd) == -1)
+		perror_return("-minishell: close", EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
 uint8_t	apply_in_redirection(t_cmd *cmd)
 {
-	int		in_fd;
 	t_list	*current_redir;
 	t_redir	*redir;
-	uint8_t result = EXIT_SUCCESS;
+	int		in_fd;
+	uint8_t	result;
 
+	result = EXIT_SUCCESS;
 	if (!cmd->redirs)
 		return (EXIT_SUCCESS);
-
 	current_redir = cmd->redirs;
 	while (current_redir)
 	{
 		redir = (t_redir *)current_redir->content;
 		if (redir->type == R_INPUT)
 		{
-			in_fd = open(redir->filename, O_RDONLY);
+			in_fd = open_input_file(redir->filename);
 			if (in_fd < 0)
-			{
-				print_error("-minishell: ");
-				perror(redir->filename);
 				return (EXIT_FAILURE);
-			}
-			if (dup2(in_fd, STDIN_FILENO) == -1)
-			{
-				if (close(in_fd) == -1)
-					perror_return("-minishell: close", EXIT_FAILURE);
-				perror_return("-minishell: dup2", EXIT_FAILURE);
-			}
-			if (close(in_fd) == -1)
-				perror_return("-minishell: close", EXIT_FAILURE);
-
-			// Continue to the next redirection if there is one.
-			result = EXIT_SUCCESS;
+			result = apply_input_fd(in_fd);
+			if (result != EXIT_SUCCESS)
+				return (result);
 		}
 		current_redir = current_redir->next;
 	}
-
 	return (result);
 }
