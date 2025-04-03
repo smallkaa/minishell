d6170be2fb11d189efa5a29e3755dd3fd88467b1
Ilia Munaev <ilyamunaev@gmail.com>
2025-04-03 07:34:38 +0300

    Refactor: exec_in_child_process.c

--- src/executor/pipeline/exec_in_child_process.c
+++ src/executor/pipeline/exec_in_child_process.c
@@ -17,39 +17,28 @@ static uint8_t	validate_dots(t_cmd *cmd)
 }
 uint8_t update_shlvl(t_cmd *cmd)
 {
-    int shlvl;
-    char *str_shlvl = ms_getenv(cmd->minishell, "SHLVL");
-    char *new_shlvl;
+	int shlvl;
+	char *str_shlvl;
+	char *new_shlvl;
 
-    if (!str_shlvl)
-        shlvl = 1; // If SHLVL doesn't exist, initialize it to 1
-    else
-        shlvl = ft_atoi(str_shlvl);
-
-    shlvl++; // Increment SHLVL
-
-    new_shlvl = ft_itoa(shlvl);
-    if (!new_shlvl)
-        return (EXIT_FAILURE);
-
-    set_variable(cmd->minishell, "SHLVL", new_shlvl, 1); // Save the updated SHLVL
-    free(new_shlvl);
-
-    // Make sure to update the environment array
-    update_env(cmd->minishell);
-
-    return (EXIT_SUCCESS);
+	str_shlvl = ms_getenv(cmd->minishell, "SHLVL");
+	if (!str_shlvl)
+		shlvl = 1;
+	else
+		shlvl = ft_atoi(str_shlvl);
+	shlvl++;
+	new_shlvl = ft_itoa(shlvl);
+	if (!new_shlvl)
+		return (EXIT_FAILURE);
+	set_variable(cmd->minishell, "SHLVL", new_shlvl, 1);
+	free(new_shlvl);
+	update_env(cmd->minishell);
+	return (EXIT_SUCCESS);
 }
 
-
-
-int is_minishell_executable(t_cmd *cmd)
+bool is_minishell_executable(t_cmd *cmd)
 {
-    if (ft_strcmp(cmd->argv[0], "./minishell") == 0)
-	{
-        return (1);
-	}
-    return (0);
+	return (ft_strcmp(cmd->argv[0], "./minishell") == 0);
 }
 
 static void	execute_command(t_cmd *cmd)
@@ -61,7 +50,6 @@ static void	execute_command(t_cmd *cmd)
 		if (update_shlvl(cmd) == EXIT_FAILURE)
 			_exit(EXIT_FAILURE);
 	}
-
 	if(ft_strcmp(cmd->argv[0], "") == 0)
 	{
 		print_error("Command '' not found\n");
@@ -93,73 +81,44 @@ static void	execute_command(t_cmd *cmd)
 	child_execve_error(cmd);
 }
 
-// static int	handle_dup_and_close(int in_fd, int *fds, t_cmd *cmd)
-// {
-// 	if (apply_redirections(cmd) != EXIT_SUCCESS)
-// 		return (-1);
-
-// 	if (!cmd->in_redir && in_fd != STDIN_FILENO)
-// 	{
-// 		if (dup2(in_fd, STDIN_FILENO) == -1)
-// 			return (-1);
-// 		close(in_fd);
-// 	}
-// 	if (!cmd->out_redir && cmd->next && dup2(fds[1], STDOUT_FILENO) == -1)
-// 		return (-1);
-
-// 	if (fds[0] != -1)
-// 		close(fds[0]);
-// 	if (fds[1] != -1)
-// 		close(fds[1]);
-// 	return (EXIT_SUCCESS);
-// }
 bool cmd_has_input_redirection(t_cmd *cmd)
 {
-    t_list  *node;
-    t_redir *redir;
+	t_list  *node;
+	t_redir *redir;
 
-    if (!cmd || !cmd->redirs)
-        return false;
-
-    node = cmd->redirs;
-    while (node)
-    {
-        redir = (t_redir *)node->content;
-        if (redir->type == R_INPUT)
-            return true; // Found an R_INPUT
-        node = node->next;
-    }
-    return false;
+	if (!cmd || !cmd->redirs)
+		return false;
+	node = cmd->redirs;
+	while (node)
+	{
+		redir = (t_redir *)node->content;
+		if (redir->type == R_INPUT)
+			return (true);
+		node = node->next;
+	}
+	return (false);
 }
 static int	handle_dup_and_close(int in_fd, int *fds, t_cmd *cmd)
 {
-    // 1) If there's a next command, set the pipe for STDOUT first
-    if (cmd->next && dup2(fds[1], STDOUT_FILENO) == -1)
-        return (-1);
+	bool	has_in_redir;
 
-    // 2) Now apply all redirections (which may override the pipe)
-    if (apply_redirections(cmd) != EXIT_SUCCESS)
-        return (-1);
-
-    // 3) If there's no `<` redirection, but we have leftover `in_fd`, use that for STDIN
-    bool has_in_redir = cmd_has_input_redirection(cmd);
-    if (!has_in_redir && in_fd != STDIN_FILENO)
-    {
-        if (dup2(in_fd, STDIN_FILENO) == -1)
-            return (-1);
-        close(in_fd);
-    }
-
-    // 4) Close the pipe ends
-    if (fds[0] != -1)
-        close(fds[0]);
-    if (fds[1] != -1)
-        close(fds[1]);
-
-    return (EXIT_SUCCESS);
+	if (cmd->next && dup2(fds[1], STDOUT_FILENO) == -1)
+		return (-1);
+	if (apply_redirections(cmd) != EXIT_SUCCESS)
+		return (-1);
+	has_in_redir = cmd_has_input_redirection(cmd);
+	if (!has_in_redir && in_fd != STDIN_FILENO)
+	{
+		if (dup2(in_fd, STDIN_FILENO) == -1)
+			return (-1);
+		close(in_fd);
+	}
+	if (fds[0] != -1)
+		close(fds[0]);
+	if (fds[1] != -1)
+		close(fds[1]);
+	return (EXIT_SUCCESS);
 }
-
-
 
 static int	handle_child(t_cmd *cmd, int in_fd, int *fds)
 {
@@ -171,7 +130,9 @@ static int	handle_child(t_cmd *cmd, int in_fd, int *fds)
 
 static int	wait_for_children(pid_t *pids, int count, uint8_t *exit_status)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (i < count)
 	{
 		int status;
@@ -237,26 +198,25 @@ static bool	check_pipeline_limit(int cmd_count)
 
 uint8_t	exec_in_child_process(t_cmd *cmd)
 {
-	uint8_t		exit_status = EXIT_FAILURE;
-	int			in_fd = STDIN_FILENO;
+	uint8_t		exit_status;
+	int			in_fd;
 	int			fds[2];
-	int			cmd_count = 0;
+	int			cmd_count;
 	pid_t		pids[MAX_CMDS];
 
+	exit_status = EXIT_FAILURE;
+	in_fd = STDIN_FILENO;
+	cmd_count = 0;
 	while (cmd)
 	{
 		if (check_pipeline_limit(cmd_count))
 			return (EXIT_FAILURE);
-
 		if (create_pipe_if_needed(cmd, fds) == -1)
 			return (EXIT_FAILURE);
-
 		pids[cmd_count] = fork_command(cmd, in_fd, fds);
 		if (pids[cmd_count] == -1)
 			return (EXIT_FAILURE);
-
 		cleanup_parent_fds(&in_fd, fds);
-
 		cmd = cmd->next;
 		cmd_count++;
 	}
