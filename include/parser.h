72050a43f6bc3cb0637cf943d380028fedc4b443
Pavel Vershinin <pvershin@student.hive.fi>
2025-04-24 14:26:16 +0300

    Norm: new functions and one header

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: pvershin <pvershin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 21:20:20 by pvershin          #+#    #+#             */
-/*   Updated: 2025/04/23 21:20:21 by pvershin         ###   ########.fr       */
+/*   Updated: 2025/04/24 14:21:15 by pvershin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PARSER_H
 
 # include <minishell.h>
+
 typedef struct s_wordinfo
 {
 	char	*grouped;
@@ -42,18 +43,20 @@ typedef struct s_parse_ctx
 	t_redir_ctx		redir;
 	int				i;
 }	t_parse_ctx;
-bool	is_skippable_token(t_Token *tok);
-void	process_non_word(t_TokenArray *new_tokens, int *j, t_Token *tok);
-void	process_word(t_TokenArray *new_tokens,
-	t_TokenArray *old_tokens, int *i, int *j);
-int	count_new_tokens(t_TokenArray *tokens);
-void	process_pipe_token(t_cmd **current, int *i);
-int	init_redir_command_if_needed(t_redir_ctx *ctx);
-int	apply_redirection(t_redir_ctx *ctx, t_TokenType type);
+bool			is_skippable_token(t_Token *tok);
+void			process_non_word(t_TokenArray *new_tokens, int *j, t_Token \
+	*tok);
+void			process_word(t_TokenArray *new_tokens,
+					t_TokenArray *old_tokens, int *i, int *j);
+int				count_new_tokens(t_TokenArray *tokens);
+void			process_pipe_token(t_cmd **current, int *i);
+int				init_redir_command_if_needed(t_redir_ctx *ctx);
+int				apply_redirection(t_redir_ctx *ctx, t_TokenType type);
 t_TokenArray	*token_array_init(void);
-void	token_array_add(t_TokenArray *array, t_Token token);
-void	token_array_free(t_TokenArray *array);
-int	group_word_tokens(t_TokenArray *tokens);
-t_cmd	*create_command_from_tokens(t_mshell *shell, t_TokenArray *tokens);
+void			token_array_add(t_TokenArray *array, t_Token token);
+void			token_array_free(t_TokenArray *array);
+int				group_word_tokens(t_TokenArray *tokens);
+t_cmd			*create_command_from_tokens(t_mshell *shell, t_TokenArray\
+	*tokens);
 
 #endif
--- src/parser/parser_helpers2.c
+++ src/parser/parser_helpers2.c
@@ -6,7 +6,7 @@
 /*   By: pvershin <pvershin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 14:00:16 by pvershin          #+#    #+#             */
-/*   Updated: 2025/04/24 14:06:35 by pvershin         ###   ########.fr       */
+/*   Updated: 2025/04/24 14:17:01 by pvershin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,25 @@
 
 void	free_cmd_list(t_list **cmd_list)
 {
-    t_list *current;
-    t_list *next;
-    t_cmd  *cmd;
+	t_list	*current;
+	t_list	*next;
+	t_cmd	*cmd;
 
-    if (!cmd_list || !*cmd_list)
-        return;
-
-    current = *cmd_list;
-    while (current)
-    {
-        next = current->next; // Store pointer to the next node
-
-        // Free the content (t_cmd struct and its internals)
-        cmd = (t_cmd *)current->content;
-        if (cmd) {
-             free_cmd(cmd); // Use your existing function to free t_cmd
-        }
-
-        // Free the t_list node itself
-        free(current);
-
-        current = next; // Move to the next node
-    }
-    *cmd_list = NULL; // Set the head pointer to NULL as the list is empty
+	if (!cmd_list || !*cmd_list)
+		return ;
+	current = *cmd_list;
+	while (current)
+	{
+		next = current->next;
+		cmd = (t_cmd *)current->content;
+		if (cmd)
+		{
+			free_cmd(cmd);
+		}
+		free(current);
+		current = next;
+	}
+	*cmd_list = NULL;
 }
 
 // parser_helpers.c
