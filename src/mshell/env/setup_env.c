c568af230959adc0a06758f971295bfcc6dd2ac0
Ilia Munaev <ilyamunaev@gmail.com>
2025-05-07 17:14:01 +0300

    Fix: v.2 conditional jump in allocate_minishell

--- src/mshell/env/setup_env.c
+++ src/mshell/env/setup_env.c
@@ -6,7 +6,7 @@
 /*   By: Ilia Munaev <ilyamunaev@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 14:49:47 by Ilia Munaev       #+#    #+#             */
-/*   Updated: 2025/05/07 17:12:16 by Ilia Munaev      ###   ########.fr       */
+/*   Updated: 2025/05/07 17:13:44 by Ilia Munaev      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,8 +59,9 @@ char	**setup_env(char **envp)
 	int		envp_len;
 	char	**env;
 
-	envp_len = ft_arr_size(envp);rror (init_env): failed to setup envir
+	envp_len = ft_arr_size(envp);
 	env = malloc((envp_len + 1) * sizeof(char *));
+	env = NULL;
 	if (!env)
 	{
 		print_error("Error (setup_envp): env malloc failed\n");
